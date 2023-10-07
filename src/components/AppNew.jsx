9f9d56a0a2398825bdb01d300286b648bd6e23ef
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2023-10-07 19:50:10 +0200

    app

--- src/components/AppNew.jsx
+++ src/components/AppNew.jsx
@@ -1,8 +1,19 @@
 // import { ContactForm } from './ContactForm/ContactForm';
+import { useDispatch, useSelector } from 'react-redux';
 import { ContactList } from './ContactList/ContactList';
+import { useEffect } from 'react';
+import { fetchContacts } from './redux/operations';
 // import { Filter } from './Filter/Filter';
 
 export const AppNew = () => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+
+
   return (
     <div>
       <h1>Phonebook</h1>
